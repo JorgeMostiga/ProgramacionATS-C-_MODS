f18b33c488bc70199246568815b044f851a51c83
JorgeMostiga <jorgemostiga@outlook.es>
2025-03-17 17:58:11 -0500

    Código sobre Búsquedas Binarias
    Mi Versión sobre el código de Búsquedas binarias en un arreglo. Este código pide al usuario que ingrese un número cualquiera y luego lo busca en un arreglo de 5 elementos (4 indices). Se puede modificar para que el usuario pueda llenar los datos del arreglo, comenten si les gustaría que lo modifique.

---
+++ 54-BusquedaBinaria.cpp
@@ -0,0 +1,72 @@
+/*
+    Busqueda Binaria By Alejandro Tavoada
+    Url video explicaod por Alejandro: https://www.youtube.com/watch?v=pxh4QFzDh-Q
+    [Mod by Jorge Móstiga]
+*/
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int numeros[] = {1, 2, 3, 4, 5};
+    int inf, sup, mitad, dato;
+    char band = 'F'; // Puede tomar 3 valores (F, V, E) (Falso, Verdadero, Encontrado) ya que sin solo fuera 2 valores(V o F) sería de tipo boolean
+    dato = 3;
+    // Algoritmo de la Busqueda Binaria
+    inf = 0;
+    sup = 5;
+
+    // averiguando si el dato existe en el arreglo:
+    for (int i = 0; i < 5; i++)
+    {
+        if (dato == numeros[i])
+        {
+            band = 'E';
+            cout << "EL Numero si esta en el arreglo, Hallando la Posicion en la que esta dentro del arreglo:\n";
+            system("pause");
+            break;
+        }
+    }
+    // Si existe (band=='E')
+    if (band == 'E')
+    {
+        while (inf <= sup)
+        {
+            mitad = (inf + sup) / 2;
+
+            if (numeros[mitad] == dato)
+            {
+                band = 'V';
+                break;
+            }
+            else if (numeros[mitad] > dato)
+            {
+                sup = mitad;
+                mitad = (inf + sup) / 2;
+            }
+            else if (numeros[mitad] < dato)
+            {
+                inf = mitad;
+
+                mitad = (inf + sup) / 2;
+            }
+            else
+            {
+                cout << "Ups!";
+            }
+        }
+    }
+    // Fin del Algoritmo Busqueda Binaria
+    if (band == 'V')
+    {
+        cout << "El numero a sido encontrado en la Posicion: " << mitad << endl;
+    }
+    else
+    {
+        cout << "No existe ese numero en el arreglo\n";
+    }
+    // system("pause");
+    cout<<"Presiona una tecla para terminar el programa.";
+    getch();
+    return 0;
+}
